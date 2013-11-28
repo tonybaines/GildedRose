7670b90c12b0fb4dc3e5ca7852a251693f9bf418
Tony Baines <tony.baines.1@gmail.com>
2013-11-28 09:40:45 +0000

    Use local libs in the project definitions for Eclipse and IDEA

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@
 PACKAGE="refactoring-kata"
 rm -rf $PACKAGE $PACKAGE.zip
 mkdir $PACKAGE
-cp -a README.md .project .classpath src pom.xml $PACKAGE
+gradle copyToLib
+cp -a README.md .project .classpath src lib *.iml $PACKAGE
 zip -r $PACKAGE.zip $PACKAGE
 rm -rf $PACKAGE
