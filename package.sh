6077dc4148efe9a27891dc697f8fa01e03ac1073
Tony Baines <tony.baines.1@gmail.com>
2013-11-28 09:29:42 +0000

    Migrated the build to Gradle

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@
 PACKAGE="refactoring-kata"
 rm -rf $PACKAGE $PACKAGE.zip
 mkdir $PACKAGE
-cp -a README .project .classpath src pom.xml $PACKAGE
+cp -a README.md .project .classpath src pom.xml $PACKAGE
 zip -r $PACKAGE.zip $PACKAGE
 rm -rf $PACKAGE
